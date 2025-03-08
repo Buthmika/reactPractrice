7ecc79afbcb0cea7bb62cee386db64ccdd84c7ae
Buthmika <buthmikamenushan@gmail.com>
2025-03-08 17:39:25 +0530

    Update login.jsx

--- login/src/components/login/login.jsx
+++ login/src/components/login/login.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import './login.css'
+import user_icon from '..assets/person.png'
+import password_icon from '..assets/password.png'
+import email_icon from '..assets/email.png'
 
 const login = () => {
   return (
