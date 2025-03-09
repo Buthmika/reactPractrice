d3400f94e0198ea404549c4d001f2f575d956625
Buthmika <buthmikamenushan@gmail.com>
2025-03-09 10:26:54 +0530

    update

--- loginSignup/login-signup/src/components/LoginSignup/LoginSignup.jsx
+++ loginSignup/login-signup/src/components/LoginSignup/LoginSignup.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import './LoginSignup.css'
+import user_icon from '../Assets/person.png';
+import email_icon from '../Assets/email.png';
+import password_icon from '../Assets/password.png';
 
 const LoginSignup = () => {
   return (
