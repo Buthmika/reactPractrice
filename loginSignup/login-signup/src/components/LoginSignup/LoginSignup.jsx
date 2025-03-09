b08b3011568afc7db1fae2b782db2066a3ca42ee
Buthmika <buthmikamenushan@gmail.com>
2025-03-09 11:29:26 +0530

    Update LoginSignup.jsx

--- loginSignup/login-signup/src/components/LoginSignup/LoginSignup.jsx
+++ loginSignup/login-signup/src/components/LoginSignup/LoginSignup.jsx
@@ -2,7 +2,6 @@ import React, { use } from 'react'
 import './LoginSignup.css'
 import user_icon from '../Assets/person.png';
 import email_icon from '../Assets/email.png';
-import password_icon from '../Assets/password.png';
 
 const LoginSignup = () => {
       const[action,setAction]=useState("signup");
