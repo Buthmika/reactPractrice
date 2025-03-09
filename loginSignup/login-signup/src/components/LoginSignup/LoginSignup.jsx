ab5bbc6cb53e73148e4b99c6de396a6116c8b768
Buthmika <buthmikamenushan@gmail.com>
2025-03-09 10:35:43 +0530

    Update LoginSignup.jsx

--- loginSignup/login-signup/src/components/LoginSignup/LoginSignup.jsx
+++ loginSignup/login-signup/src/components/LoginSignup/LoginSignup.jsx
@@ -6,7 +6,24 @@ import password_icon from '../Assets/password.png';
 
 const LoginSignup = () => {
   return (
-    <div>
+    <div className='container'>
+      <div className="header">
+            <div className="text">Sign up</div>
+            <div className="underline"></div>
+      </div>
+      <div className="inputs"></div>
+      <div className="input">
+            <img src={user_icon} alt=" "/>
+            <input type="text" />
+      </div>
+      <div className="input">
+            <img src={email_icon} alt=" "/>
+            <input type="email" />
+      </div>
+      <div className="input">
+            <img src={password_icon} alt=" "/>
+            <input type="password" />
+      </div>
       
     </div>
   )
