c4decb737d6b48f05e0995f8fd63cf3023c5423d
Buthmika <buthmikamenushan@gmail.com>
2025-03-10 15:27:07 +0530

    Update LoginSignup.jsx

--- loginSignup/login-signup/src/components/LoginSignup/LoginSignup.jsx
+++ loginSignup/login-signup/src/components/LoginSignup/LoginSignup.jsx
@@ -5,11 +5,11 @@ import email_icon from '../Assets/email.png';
 import password_icon from '../Assets/password.png';
 
 const LoginSignup = () => {
-     
+  const[action,setAction]=useState("Sign Up")   
   return (
     <div className='container'>
       <div className="header">
-            <div className="text">Sign up</div>
+            <div className="text">{action}</div>
             <div className="underline"></div>
       </div>
 <div className="inputs">
