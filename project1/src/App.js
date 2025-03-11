f36917c293f1dc12501b4c4b39eb51370901d68b
Buthmika <buthmikamenushan@gmail.com>
2025-03-11 18:19:14 +0530

    Update App.js

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import PartOne from './components/PartOne';
 function App() {
   return (
     <div className="App">
       <h1>This is my first react project</h1>
-      <partOne/>
+      <PartOne/>
     
     </div>
   );
