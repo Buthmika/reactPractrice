0a1d25948ed2d95dbc343f3d2fb92fca0f81b0a4
Buthmika <buthmikamenushan@gmail.com>
2025-03-12 06:35:55 +0530

    Update TopBox.js

--- project1/src/components/TopBox.js
+++ project1/src/components/TopBox.js
@@ -3,7 +3,9 @@ import React from 'react'
 export default function TopBox() {
   return (
     <div>
-      
+      <div className='topicBox'>
+            <span>This is the topic</span>
+      </div>
     </div>
   )
 }
