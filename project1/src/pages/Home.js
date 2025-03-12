5c620df2ab7978e9f2176a33d941c52c0cbc3f2d
Buthmika <buthmikamenushan@gmail.com>
2025-03-12 06:37:45 +0530

    Update Home.js

--- project1/src/pages/Home.js
+++ project1/src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import TopBox from '../components/TopBox'
 
 export default function Home() {
   return (
     <div>
       <h1>This is Home Page</h1>
+      <TopBox/>
     </div>
   )
 }
