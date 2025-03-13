95344b765fc0535854bcc11a69bdc32e24a0ad37
Buthmika <buthmikamenushan@gmail.com>
2025-03-13 11:26:51 +0530

    update

--- project1/src/components/TopBox.js
+++ project1/src/components/TopBox.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import "./TopBox.css"
 export default function TopBox() {
-  const topic="My name is buthmika"
+  const topic="Food is buthmika"
   return (
     <div>
       <div className='topicBox'>
-            <span className='text'>{alert("Hello world")}</span>
+            <span className='text'>{topic}</span>
       </div>
     </div>
   )
--- project1/src/pages/Home.js
+++ project1/src/pages/Home.js
@@ -6,8 +6,10 @@ export default function Home() {
   return (
     <div>
       <h1>Home Page</h1>
-      <TopBox/>
-      <Content/>
+      <TopBox food="Kottu"/ >
+      <TopBox food="Kottu"/>
+      <TopBox food="Kottu"/>
+      
     </div>
   )
 }
