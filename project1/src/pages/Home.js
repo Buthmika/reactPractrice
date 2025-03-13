9100b7f082c86468238ad91603b90cd2842ff592
Buthmika <buthmikamenushan@gmail.com>
2025-03-13 11:38:25 +0530

    Update Home.js

--- project1/src/pages/Home.js
+++ project1/src/pages/Home.js
@@ -6,9 +6,9 @@ export default function Home() {
   return (
     <div>
       <h1>Home Page</h1>
-      <TopBox food="Kottu"/ >
-      <TopBox food="Rice"/>
-      <TopBox food="Noodles"/>
+      <TopBox food="Kottu" price="Rs.200"/ >
+      <TopBox food="Rice" price="Rs.100"/>
+      <TopBox food="Noodles" price="Rs.300"/>
       
     </div>
   )
