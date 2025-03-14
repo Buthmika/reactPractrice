3b16ec35be0bfa3d833d444c117500c09966ec7b
Buthmika <buthmikamenushan@gmail.com>
2025-03-14 10:01:01 +0530

    Update App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -9,13 +9,24 @@ function App() {
   const hello2=()=>{
     console.log("Hello 2")
   }
+  function hello3(name){
+    console.log("Hello 3"+ name);
+  }
 
 
   return (
     <>
       <div className="wrapper">
         <button onClick={hello}>Click 1</button>
-        <button onClick={hello2}>Click 1</button>
+        <button onClick={hello2}>Click 2</button>
+        <button onClick={
+          ()=>{
+            console.log(hello3("Buthmika"))
+          }
+          // hello3("Buthmika")
+          }>Click 3</button>
+          
+
       </div>
     </>
   )
