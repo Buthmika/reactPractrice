f73e033f1760d57f60953d9fe97636a2c58656e4
Buthmika <buthmikamenushan@gmail.com>
2025-03-16 11:18:43 +0530

    Update App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,14 +1,20 @@
 import './App.css'
 function App() {
-  
+  const firstName="Buthmika";
+  const secondName="Menushan"
+  const surName="Dampitige";
   const a=10;
+  const getFunction=(f,m,s)=>{
+      return `${f} ${m} ${s}`;
+  }
 
   return (
     <>
       <div className="cont">
         <h1>React App</h1>
-        <p>Buthmika menushan {5*a}</p>
+        <p>{`${firstName}  ${secondName}`} {5*a}</p>
         <p>{a>50?"greater":"lesser"}</p>
+        <p>Full name {getFunction("Methula","Sethmika","Dampitige")}</p>
       </div>
     </>
   )
