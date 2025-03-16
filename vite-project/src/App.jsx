2c554aa6bb87e669af3c5b0793daa2c42a8f1805
Buthmika <buthmikamenushan@gmail.com>
2025-03-16 11:05:59 +0530

    Update App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 function App() {
   
-
+  const a=10;
 
   return (
     <>
       <div className="cont">
         <h1>React App</h1>
+        <p>Buthmika menushan {5*a}</p>
+        <p>{a>50?"greater":"lesser"}</p>
       </div>
     </>
   )
