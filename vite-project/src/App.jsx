f71bab23b6eb272a83509caa93b0b980389fc4a7
Buthmika <buthmikamenushan@gmail.com>
2025-03-16 11:00:56 +0530

    update

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,31 +1,12 @@
-
 import './App.css'
-import Nav from './Components/Nav'
-
 function App() {
-  function hello(){
-    console.log("Hello 1");
-  }
-  const hello2=()=>{
-    console.log("Hello 2")
-  }
-  function hello3(name,e){
-    console.log("Hello 3"+ name,e);
-  }
-  const hello4=()=>{
-    console.log("Hello 4" +name)
-  }
+  
 
 
   return (
     <>
-      <div className="wrapper">
-        <button onClick={hello}>Click 1</button>
-        <button onClick={hello2}>Click 2</button>
-        <button onClick={(e)=>{console.log(hello3("Buthmika",e.target))}}>Click 3</button>
-        <button onClick={()=>{console.log(hello4("Menushan"))}}>Click 4</button>
-          
-
+      <div className="cont">
+        <h1>React App</h1>
       </div>
     </>
   )
