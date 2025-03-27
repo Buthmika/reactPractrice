7e675c657cbe56c27183e96c11106a11b2f5d5ce
Buthmika <buthmikamenushan@gmail.com>
2025-03-27 19:52:44 +0530

    Update Arraymap.jsx

---
+++ vite-project/src/Components/Arraymap.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Arraymap({nameList}) {
+  return (
+    <div>
+            <h1>Full Name List(Map)</h1>
+            <ul>
+                {
+                  nameList.map((name,index)=>{
+                        return <li key={index}>{name}</li>
+                  })
+                        
+                  }
+                  
+            </ul>
+    </div>
+  )
+}
+
+export default Arraymap
