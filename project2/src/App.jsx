01b4423d13e1473c978f00d2b8f4fbcf9e853d3a
Buthmika <buthmikamenushan@gmail.com>
2025-03-30 16:24:27 +0530

    Update App.jsx

---
+++ project2/src/App.jsx
@@ -0,0 +1,24 @@
+
+
+
+import './App.css'
+import ArrayFilter from './components/ArrayFilter';
+import ArrayMap from './components/ArrayMap'
+import ArrayReduce from './components/ArrayReduce';
+const names=['React','Vue','Angular','Svelte','Ember'];
+const numbers=[1,2,3,4,5,6,7,8,9,10];
+
+function App() {
+
+
+  return (
+    <>
+      <h1>hi</h1>
+      <ArrayMap nameList={names}></ArrayMap>
+      <ArrayFilter nameList={names}></ArrayFilter>
+      <ArrayReduce numberList={numbers}></ArrayReduce>
+    </>
+  )
+}
+
+export default App
