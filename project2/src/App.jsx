f9a9c4142dca1314827ddf8560b52d4df27d7fc0
Buthmika <buthmikamenushan@gmail.com>
2025-03-30 16:08:23 +0530

    update

--- project2/src/App.jsx
+++ project2/src/App.jsx
@@ -4,7 +4,9 @@
 import './App.css'
 import ArrayFilter from './components/ArrayFilter';
 import ArrayMap from './components/ArrayMap'
+import ArrayReduce from './components/ArrayReduce';
 const names=['React','Vue','Angular','Svelte','Ember'];
+const numbers=[1,2,3,4,5,6,7,8,9,10];
 
 function App() {
 
@@ -14,6 +16,7 @@ function App() {
       <h1>hi</h1>
       <ArrayMap nameList={names}></ArrayMap>
       <ArrayFilter nameList={names}></ArrayFilter>
+      <ArrayReduce></ArrayReduce>
     </>
   )
 }
---
+++ project2/src/components/ArrayReduce.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function ArrayReduce({nameList}) {
+      const filterdNames=nameList.filter((name)=>(name.startsWith('A')));
+  return (
+    <div>
+            <h1>Filterd Name List(Filter)(Map)</h1>
+      <ul>
+        {
+          filterdNames.map((name,index)=>{
+            return(
+              <li key={index}>{name}</li>
+            )
+          })
+      
+          
+        }
+      </ul>
+    </div>
+  )
+}
+
+export default ArrayReduce
