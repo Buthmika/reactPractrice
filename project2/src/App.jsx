f8aec4cad29bb53b15d26f7037ee3bb4a6296cbe
Buthmika <buthmikamenushan@gmail.com>
2025-03-30 07:06:48 +0530

    update

--- project2/src/App.jsx
+++ project2/src/App.jsx
@@ -2,13 +2,16 @@
 
 
 import './App.css'
+import ArrayMap from './components/ArrayMap'
+const names=['React','Vue','Angular','Svelte','Ember'];
 
 function App() {
 
 
   return (
     <>
-      
+      <h1>hi</h1>
+      <ArrayMap nameList={names}></ArrayMap>
     </>
   )
 }
---
+++ project2/src/components/ArrayMap.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function ArrayMap() {
+  return (
+    <div>
+      <h1>Full Name List(Map)</h1>
+      <ul>
+        
+      </ul>
+    </div>
+  )
+}
+
+export default ArrayMap
