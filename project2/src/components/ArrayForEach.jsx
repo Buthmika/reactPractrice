456a8b7cb00ecbc5ded445e4a0e547eec6e0d40d
Buthmika <buthmikamenushan@gmail.com>
2025-04-01 15:57:42 +0530

    Update ArrayForEach.jsx

--- project2/src/components/ArrayForEach.jsx
+++ project2/src/components/ArrayForEach.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
-function ArrayForEach() {
+function ArrayForEach({nameList}) {
   return (
     <div>
       <hr />
+      <h1>Full Name List(Map)</h1>
+      <ul>
+            {nameList.map((name,index)=>(<li key={index}>{name}</li>))}
+      </ul>
     </div>
   )
 }
