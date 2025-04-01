40ffaf62a4c5d5a0c9939290c60de7749adf724b
Buthmika <buthmikamenushan@gmail.com>
2025-04-01 16:18:09 +0530

    Update ArrayForEach.jsx

--- project2/src/components/ArrayForEach.jsx
+++ project2/src/components/ArrayForEach.jsx
@@ -4,9 +4,9 @@ function ArrayForEach({nameList}) {
   return (
     <div>
       <hr />
-      <h1>Full Name List(Map)</h1>
+      <h1>Full Name List(forEach)</h1>
       <ul>
-            {nameList.map((name,index)=>(<li key={index}>{name}</li>))}
+            {nameList.forEach((name,index)=>(<li key={index}>{name}</li>))}
       </ul>
     </div>
   )
