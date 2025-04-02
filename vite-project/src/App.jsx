96d69d53ddb0c1ddedd0ddc2403759da5ccc0def
Buthmika <buthmikamenushan@gmail.com>
2025-04-02 06:22:13 +0530

    Update App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Arraymap from './Components/Arraymap'
 import ArrayFilter from './Components/ArrayFilter'
+import ReactFragment from '../../project3/vite-project/src/components/ReactFragment'
 // const name=['React','Vue','Angular','Svelte','Ember'];
 // const numbers=[1,2,3,4,5,6,7,8,9,10];
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <>
       <div className="cont">
         <h1>React App</h1>
+        <ReactFragment></ReactFragment>
        
 
         
