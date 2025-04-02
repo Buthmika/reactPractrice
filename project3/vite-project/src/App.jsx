e55b6d57bc20275ea5270dc06f2f0374f88b27d5
Buthmika <buthmikamenushan@gmail.com>
2025-04-02 06:32:07 +0530

    import reactFragment.jsx for app.jsx

--- project3/vite-project/src/App.jsx
+++ project3/vite-project/src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import ReactFragment from './components/ReactFragment'
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <h1>ho</h1>
+      <ReactFragment></ReactFragment>
     </>
   )
 }
