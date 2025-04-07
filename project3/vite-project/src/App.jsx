9d7378f48dbcb14e5d0e0fd0f5fa8903e5916462
Buthmika <buthmikamenushan@gmail.com>
2025-04-07 09:55:18 +0530

    Update App.jsx

--- project3/vite-project/src/App.jsx
+++ project3/vite-project/src/App.jsx
@@ -1,17 +1,13 @@
 
 import './App.css'
-import OrderdList from './components/OrderdList'
-import ReactFragment from './components/ReactFragment'
 
 function App() {
-  const nameList=['Amal','Kamal','Sunil','Rajith','Nimal']
 
 
   return (
     <>
       <h1>ho</h1>
-      <ReactFragment  itemList={nameList}></ReactFragment>
-      <OrderdList itemList={nameList}></OrderdList>
+      
     </>
   )
 }
