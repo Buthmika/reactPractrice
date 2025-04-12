bf0b243d354834b7444413c1433ba5483fb33ca2
Buthmika <buthmikamenushan@gmail.com>
2025-04-12 09:48:39 +0530

    Update main.jsx

--- mCodes/src/main.jsx
+++ mCodes/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import UseState from './components/UseState.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <UseState></UseState>
   </StrictMode>,
 )
