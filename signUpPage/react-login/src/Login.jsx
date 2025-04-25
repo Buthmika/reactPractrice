1d6486f0b456b60cfa64e435eeb0501eebe18bf6
Buthmika <buthmikamenushan@gmail.com>
2025-04-25 18:33:25 +0530

    create sign up structre

--- signUpPage/react-login/src/Login.jsx
+++ signUpPage/react-login/src/Login.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import { useState } from 'react';
+import axios from 'axios';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post('http://localhost/php-backend/login.php', {
+        email,
+        password
+      });
+      setMessage(res.data.message);
+    } catch (err) {
+      setMessage('Login failed');
+    }
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <input type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} required />
+      <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} required />
+      <button type="submit">Login</button>
+      <p>{message}</p>
+    </form>
+  );
 }
 
-export default Login
+export default Login;
